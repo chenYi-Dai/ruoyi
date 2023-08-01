68d638b06bd6aa2dcfd18a8feb245c1443b38ef4
chenyi <17864261507@163.com>
2023-08-01 09:42:00 +0800

    feat:新增openfeign降级功能 1.针对某个方法进行降级。如果需要全局降级需要定义个默认方法进行将处理

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/TestInfo.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/monitor/TestInfo.java
@@ -1,21 +1,64 @@
 package com.ruoyi.web.controller.monitor;
 
-import io.swagger.annotations.Api;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.ruoyi.common.entry.ResponseEntity;
+import com.ruoyi.common.feignService.OpenFeignService;
+import com.ruoyi.common.prarm.NodeListForm;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RestController
-@RequestMapping(value = "/druidinfo")
+@RequestMapping(value = "/druidData")
+//@DefaultProperties(defaultFallback = "globalFallbackMethod")
 public class TestInfo {
 
-    @ApiOperation(value = "数据监控登陆")
-    @RequestMapping(value = "/submit",method = RequestMethod.POST)
-    public void submitLogin(){
-        log.info("submitLogin");
-        System.out.println(111);
+    @Resource
+    private OpenFeignService openFeignService;
+
+    @ApiOperation(value = "查询单个数据")
+    @HystrixCommand(fallbackMethod = "tomeOutFailCallBack")
+    @RequestMapping(value = "/query/one",method = RequestMethod.POST)
+    public ResponseEntity queryOneData(@RequestBody NodeListForm nodeListForm) throws Exception {
+        log.info("submitLogin request |{}",nodeListForm);
+        ResponseEntity responseEntity = openFeignService.queryOneData(nodeListForm);
+        return responseEntity;
+    }
+
+    @ApiOperation(value = "查询所有列表")
+    @HystrixCommand(fallbackMethod = "globalFallbackMethod")
+    @RequestMapping(value = "/query/list",method = RequestMethod.POST)
+    public ResponseEntity queryAllData(@RequestBody NodeListForm nodeListForm) throws Exception {
+        log.info("submitLogin request |{}",nodeListForm);
+        ResponseEntity responseEntity = openFeignService.queryAllData(nodeListForm);
+        return responseEntity;
+    }
+
+    public ResponseEntity tomeOutFailCallBack(@RequestBody NodeListForm nodeListForm){
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCode(500);
+        responseEntity.setMessage(nodeListForm.getSpaceSetId() + "服务超时");
+        Map map  = new HashMap<>();
+        map.put("msg","tomeOutFailCallBack 服务超时 处理");
+        responseEntity.setValue(map);
+        return responseEntity;
+    }
+
+    public ResponseEntity globalFallbackMethod(@RequestBody NodeListForm nodeListForm){
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setCode(500);
+        Map map  = new HashMap<>();
+        map.put("msg","globalFallbackMethod 服务超时 处理");
+        responseEntity.setValue(map);
+        return responseEntity;
     }
 }
